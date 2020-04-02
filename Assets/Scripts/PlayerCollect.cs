a027fb9caaacc3adbdfa53b70114f08bd9b905ec
mksyta99 <mksyta99@gmail.com>
2020-04-02 19:22:40 +0200

    menu and animations

---
+++ Assets/Scripts/PlayerCollect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerCollect : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = (int)System.Char.GetNumericValue(text.text[2]);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.transform.tag == "Collected")
+        {
+            Destroy(other.gameObject);
+            score += 1;
+            text.text = "X " + score;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -24,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-		if (Input.GetKeyDown("up") && isGrounded)
+		if (Input.GetKeyDown("w") && isGrounded)
 		{
 			jump = true;
 		}
@@ -40,17 +40,26 @@ public class PlayerMovement : MonoBehaviour
 
 	void OnCollisionEnter2D(Collision2D other)
 	{
-		if (other.gameObject.tag == "Ground")
+		if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Moving Platform")
 		{
 			isGrounded = true;
+		}
+		if (other.gameObject.tag == "Moving Platform")
+		{
+			Transform m_platform = other.gameObject.transform;
+			transform.SetParent(m_platform);
 		}
 
 	}
 	void OnCollisionExit2D(Collision2D other)
 	{
-		if (other.gameObject.tag == "Ground")
+		if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Moving Platform")
 		{
 			isGrounded = false;
+		}
+		if(other.gameObject.tag == "Moving Platform")
+		{
+			transform.SetParent(null);
 		}
 
 	}
