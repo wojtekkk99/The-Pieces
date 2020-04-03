9f76d925e06ffb3de58f0480f9db663e187e64f2
mksyta99 <mksyta99@gmail.com>
2020-04-03 22:51:52 +0200

    intro and jumping correct

---
+++ Assets/Scripts/GoToGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToGame : MonoBehaviour
+{
+    public LevelChanger levelChanger;
+
+    // Update is called once per frame
+    public void goToGame()
+    {
+        levelChanger.FadeToNextLevel(2);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,22 +14,22 @@ public class PlayerMovement : MonoBehaviour
 	bool jump = false;
 	public bool isGrounded = true;
 	Rigidbody2D rgd;
-	public GameObject animator;
 	public bool IsFreeze = false;
 	public GameObject gameOver;
 	public bool IsPaused = false;
 	public GameObject pauseScreen;
+
 	void Start()
 	{
 		rgd = GetComponent<Rigidbody2D>();
-		animator.gameObject.SetActive(false);
+		pauseScreen.gameObject.SetActive(false);
 		gameOver.SetActive(false);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-
+		
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
 		Boy_animation.SetFloat("is_stand", Mathf.Abs(horizontalMove));
@@ -42,8 +42,8 @@ public class PlayerMovement : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.Escape) && IsPaused == false)
 		{
 			GetComponent<Animator>().enabled = false;
-			animator.SetActive(true);
-			animator.GetComponent<Animator>().Play("ShowPause");
+			pauseScreen.SetActive(true);
+			pauseScreen.GetComponent<Animator>().Play("ShowPause");
 			rgd.constraints = RigidbodyConstraints2D.FreezePosition;
 			IsFreeze = true;
 		}
@@ -60,31 +60,5 @@ public class PlayerMovement : MonoBehaviour
 		// Move our character
 		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
 		jump = false;
-	}
-
-	void OnCollisionEnter2D(Collision2D other)
-	{
-		if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Moving Platform")
-		{
-			isGrounded = true;
-		}
-		if (other.gameObject.tag == "Moving Platform")
-		{
-			Transform m_platform = other.gameObject.transform;
-			transform.SetParent(m_platform);
-		}
-
-	}
-	void OnCollisionExit2D(Collision2D other)
-	{
-		if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Moving Platform")
-		{
-			isGrounded = false;
-		}
-		if(other.gameObject.tag == "Moving Platform")
-		{
-			transform.SetParent(null);
-		}
-
 	}
 }
