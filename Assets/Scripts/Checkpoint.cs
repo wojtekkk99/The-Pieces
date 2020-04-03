d769b27ea5f102a345012ea40ec5f48f010a65d4
mksyta99 <mksyta99@gmail.com>
2020-04-03 09:58:34 +0200

    update

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public PlayerCollect other;
+    public Hearts hearts;
+    public Sprite sprite;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && other.score != 0)
+        {
+            other.score -= 1;
+            other.uploadGui();
+            GameObject checkpoint = new GameObject();
+            checkpoint.transform.position = transform.position;
+            hearts.lastCheckpoint = transform.position;
+            checkpoint.transform.Rotate(-0.7f, 0f, 26f);
+            checkpoint.AddComponent<SpriteRenderer>().sprite = sprite;
+            checkpoint.transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public string gender = "XY";
+	public CharacterController2D controller;
+	public float distToGround;
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool isGrounded = true;
+	void Start()
+	{
+	
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		if (Input.GetKeyDown("w") && isGrounded)
+		{
+			jump = true;
+		}
+
+	}
+
+	void FixedUpdate()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+		jump = false;
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Moving Platform")
+		{
+			isGrounded = true;
+		}
+		if (other.gameObject.tag == "Moving Platform")
+		{
+			Transform m_platform = other.gameObject.transform;
+			transform.SetParent(m_platform);
+		}
+
+	}
+	void OnCollisionExit2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Moving Platform")
+		{
+			isGrounded = false;
+		}
+		if(other.gameObject.tag == "Moving Platform")
+		{
+			transform.SetParent(null);
+		}
+
+	}
+}
