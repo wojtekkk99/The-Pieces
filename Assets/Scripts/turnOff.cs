2b20b142515cf5727a071e2ecf451ad48bad1254
mksyta99 <mksyta99@gmail.com>
2020-04-04 14:21:39 +0200

    yes

---
+++ Assets/Scripts/turnOff.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnOff : MonoBehaviour
+{
+    public void turnOf()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
