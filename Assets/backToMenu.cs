63b64a62547bae4a2f25086c73e1e38c1b9faceb
mksyta99 <mksyta99@gmail.com>
2020-04-06 16:35:41 +0200

    FINISH

---
+++ Assets/backToMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backToMenu : MonoBehaviour
+{
+    public LevelChanger levelChanger;
+
+    void Start()
+    {
+        GetComponent<Animator>().Play("Outro1");
+    }
+    public void backMenu()
+    {
+        levelChanger.FadeToNextLevel(0);
+    }
+}
---
+++ Assets/finish.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class finish : MonoBehaviour
+{
+    public LevelChanger levelChanger;
+    public GameObject Screen;
+    public GameObject Player;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Player.GetComponent<PlayerMovement>().hasGiveKey == true)
+            {
+                Screen.transform.Find("Key").gameObject.SetActive(false);
+                Player.GetComponent<HealthScore>().SavePlayerBoy();
+
+                string boyElems = Application.persistentDataPath + "/saves.boy";
+                string girlElems = Application.persistentDataPath + "/saves.girl";
+
+                if (File.Exists(boyElems) && File.Exists(girlElems))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    FileStream streamBoy = new FileStream(boyElems, FileMode.Open);
+                    FileStream streamGirl = new FileStream(girlElems, FileMode.Open);
+
+                    PlayerDataBoy dataBoy = formatter.Deserialize(streamBoy) as PlayerDataBoy;
+                    PlayerDataBoy dataGirl = formatter.Deserialize(streamGirl) as PlayerDataBoy;
+
+                    streamBoy.Close();
+                    streamGirl.Close();
+
+                    if (dataBoy.score == 3 && dataGirl.score == 3)
+                    {
+                        levelChanger.FadeToNextLevel(9);
+                    }
+                    else
+                        levelChanger.FadeToNextLevel(8);
+                }
+
+                
+            }
+        }
+    }
+}
